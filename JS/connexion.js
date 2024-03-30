de2eb838fd279c6ca915432b900efe245e4d4e7c
clement-le-coadou <104774552+clement-le-coadou@users.noreply.github.com>
2024-03-30 16:48:58 +0100

    Update connexion.js

--- JS/connexion.js
+++ JS/connexion.js
@@ -1,5 +1,29 @@
 function connexion() {
-    var Id = document.getElementById("Id").value;
-    var Mdp = document.getElementById("Mdp").value;
-    console.log("L'Id utilisateur est "+Id);
+    var emailInput = document.getElementById('Id');
+    var passwordInput = document.getElementById('Mdp');
+    var isValid = true;
+
+    // Vérification de l'adresse e-mail
+    if (!emailInput.value || !isValidEmail(emailInput.value)) {
+        emailInput.classList.add('error');
+        isValid = false;
+    } else {
+        emailInput.classList.remove('error');
+    }
+
+    // Vérification du mot de passe
+    if (!passwordInput.value) {
+        passwordInput.classList.add('error');
+        isValid = false;
+    } else {
+        passwordInput.classList.remove('error');
+    }
+
+    return isValid;
+}
+
+function isValidEmail(email) {
+    // Expression régulière pour vérifier le format de l'e-mail
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
 }
