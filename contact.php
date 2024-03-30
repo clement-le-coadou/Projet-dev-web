8cc06e40830091b29853146f873f250473555aba
clement-le-coadou <104774552+clement-le-coadou@users.noreply.github.com>
2024-03-30 16:29:09 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -27,20 +27,20 @@
         </header>
 
         <div class="topnav">
-            <li><a href="index.html">Accueil</a></li>
+            <li><a href="index.php">Accueil</a></li>
             <li><a href="http://localhost:8080/produits.php?cat=sportives">Sportive</a></li>
             <li><a href="http://localhost:8080/produits.php?cat=coupes">Coupé</a></li>
             <li><a href="http://localhost:8080/produits.php?cat=citadines">Citadine</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="contact.php">Contact</a></li>
         </div>
 
         <div class="content">
             <div class="left_nav">
-                <li><a href="index.html">Accueil</a></li>
+                <li><a href="index.php">Accueil</a></li>
                 <li><a href="http://localhost:8080/produits.php?cat=sportives">Sportive</a></li>
                 <li><a href="http://localhost:8080/produits.php?cat=coupes">Coupé</a></li>
                 <li><a href="http://localhost:8080/produits.php?cat=citadines">Citadine</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="contact.php">Contact</a></li>
             </div>
             <div id="contact">           
                 <form action="" method="POST" onsubmit="return validateContactForm()">
