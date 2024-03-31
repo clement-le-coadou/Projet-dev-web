5c2b4fe9a5004c0524a863758d0d50b625f20784
clement-le-coadou <104774552+clement-le-coadou@users.noreply.github.com>
2024-03-31 09:28:51 +0200

    Create bdd.php

---
+++ php/bdd.php
@@ -0,0 +1,25 @@
+<?php
+// Connexion à la base de données SQLite
+$bdd = new PDO('sqlite:bdd.db');
+
+// Requête pour récupérer les données de la base de données
+$sql = "SELECT * FROM voitures";
+$result = $bdd->query($sql);
+
+// Initialisation de la variable de session
+$_SESSION['categories'] = array();
+
+// Traitement des résultats
+if ($result !== false) {
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $categorie = $row['categorie'];
+        unset($row['categorie']); // Supprimer la colonne 'categorie' car elle est déjà dans la clé de tableau
+        $_SESSION['categories'][$categorie][] = $row;
+    }
+} else {
+    echo "Erreur lors de l'exécution de la requête";
+}
+
+// Fermeture de la connexion à la base de données
+$bdd = null;
+?>
